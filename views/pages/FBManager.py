e631e913f95f207b611254ae8f600bd8bde5ea0f
ThanhNguyen0311 <nct031194@icloud.com>
2023-12-23 20:08:25 +0700

    no message

--- views/pages/FBManager.py
+++ views/pages/FBManager.py
@@ -9,10 +9,11 @@ class FBManager(tk.Toplevel):
         self.geometry("800x600")
         self.is_open = True
         self.protocol("WM_DELETE_WINDOW", lambda: self.on_close())
+        self.fb_account_list = FBAccountsList(self)
 
         button_frame = tk.Frame(self)
         button_frame.pack(padx=5, pady=20, anchor=tk.NW)
-        button_add = tk.Button(button_frame, text="ADD", width=10, height=2)
+        button_add = tk.Button(button_frame, text="ADD", width=10, height=2, command=self.fb_account_list.show_selected)
         button_add.grid(row=0, column=0, padx=5)
         button_refresh = tk.Button(button_frame, text="REFRESH", width=10, height=2)
         button_refresh.grid(row=0, column=1, padx=5)
@@ -21,8 +22,10 @@ class FBManager(tk.Toplevel):
         button_action = tk.Button(button_frame, text="RUN TASK", width=10, height=2)
         button_action.grid(row=0, column=3, padx=5)
 
-        self.fb_account_list = FBAccountsList(self)
-        self.fb_account_list.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
+        s = ttk.Style()
+        s.theme_use('clam')
+        s.configure('Treeview', rowheight=25)
+        self.fb_account_list.pack(padx=10, pady=5, fill=tk.BOTH, expand=True)
 
     def on_close(self):
         self.destroy()
@@ -31,14 +34,9 @@ class FBManager(tk.Toplevel):
 
 class FBAccountsList(ttk.Treeview):
     def __init__(self, master):
-        ttk.Treeview.__init__(self, master, columns=("ID", "email", "password", "device", "status", "action"),
+        ttk.Treeview.__init__(self, master,
+                              columns=("ID", "email", "password", "device", "status", "action"),
                               show="headings", selectmode="extended")
-        # use tags to set the checkbox state
-        self.tag_configure('checked', image='checked')
-        self.tag_configure('unchecked', image='unchecked')
-        self.frame = ttk.Frame(self)
-        self.frame.grid(row=0, column=0, sticky="w", padx=50)
-        ttk.Button(self.frame, text="twatwa").pack()
         self.heading("ID", text="ID")
         self.heading("email", text="Email")
         self.heading("password", text="Password")
@@ -54,11 +52,14 @@ class FBAccountsList(ttk.Treeview):
         self.column("action", width=50, anchor='center')
 
         data = [
-            (0, 'nct031194@icloud.com', '272337839', 'IMEI: 123124512512', 'Offline'),
-            (1, 'nct031194@icloud.com', '272337839', 'IMEI: 123124512512', 'Offline'),
-            (2, 'nct031194@icloud.com', '272337839', 'IMEI: 123124512512', 'Offline'),
+            (0, 'nct031194@icloud.com', '272337839', 'IMEI: 123124512512', 'Offline')
         ]
 
         for i, (ID, email, password, device, status) in enumerate(data):
-            item_id = self.insert("", "end",
+            item_id = self.insert("", "end", iid=ID,
                                   values=(ID, email, password, device, status))
+
+    def show_selected(self):
+        for item in self.selection():
+            print(item)
+
