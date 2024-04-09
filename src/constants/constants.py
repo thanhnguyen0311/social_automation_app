2383988c5fa1eb64414bba5fa4716f6829be1cd4
MAY1\411 <nct031194@icloud.com>
2024-04-09 19:37:20 +0700

    adsa

--- src/constants/constants.py
+++ src/constants/constants.py
@@ -21,3 +21,14 @@ account_data_config = {
     'password': '272337839',
     'database': 'social_automation',
 }
+
+
+COMMENTS = ['<3', 'ib ah', '<3 <3 <3', 'ib em voi a.', 'can tu van',
+            'cần tư vấn', '.', '<3 <3', 'mình cần tư vấn ạ', 'check ib e shop',
+            'ib em ạ', ':-O', 'ib', 'ib e', 'wow <3', '.', '.', 'cho xin dia chi ah',
+            'ib em', 'ib e nha shop', '(y)', '.', 'l']
+
+LNT_COMMENTS = ['còn ko ạ', 'dep qua', 'xinhhh', 'thich qa <3', 'ib e nha tiem',
+                'gia bnhiu', 'gia bnhiu vậy tiệm', 'co giao hang ko ạ', 'xin gia',
+                'nhiều mẫu đẹp ghê', 'vàng gi vậy ạ', 'vừa mới ghé ^^']
+HCSPA_COMMENTS = ['e muốn đặt lịch', 'cần đặt lịch hôm nay', 'xin địa chỉ spa']
--- src/remote/facebook/tasks/likepost.py
+++ src/remote/facebook/tasks/likepost.py
@@ -1,8 +1,10 @@
+import random
 import time
 
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.by import By
 
+from src.constants.constants import COMMENTS
 from src.ld_manager.adb_shells import open_link
 from src.remote.facebook.login import LoginFacebook
 from src.utils.findText import find_text_in_screenshot
@@ -16,27 +18,40 @@ class LikePostFB(LoginFacebook):
     def __run__(self):
         self.driver = super().__run__()
         time.sleep(5)
-        open_link(link=self.link, device=self.data.device)
-        time.sleep(5)
-        if find_text_in_screenshot(self.driver, "Open with"):
-            self.driver.find_element(By.XPATH, '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.ScrollView/android.widget.ListView/android.widget.LinearLayout[1]/android.widget.LinearLayout').click()
-            self.driver.find_element(By.XPATH, '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.Button[2]').click()
+        for post_id in self.link:
+            open_link(link=f'https://www.facebook.com/{post_id}', device=self.data.device)
             time.sleep(5)
+            if find_text_in_screenshot(self.driver, "Open with"):
+                self.driver.find_element(By.XPATH,
+                                         '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.ScrollView/android.widget.ListView/android.widget.LinearLayout[1]/android.widget.LinearLayout').click()
+                self.driver.find_element(By.XPATH,
+                                         '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.Button[2]').click()
+                time.sleep(5)
 
-        try:
-            element = self.driver.find_element_by_android_uiautomator(
-                'new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector('
-                ').description("Like button. Double tap and hold to react."));')
+            try:
+                element = self.driver.find_element_by_android_uiautomator(
+                    'new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector('
+                    ').description("Like button. Double tap and hold to react."));')
 
-            time.sleep(3)
+                time.sleep(3)
 
-            self.driver.find_element(By.XPATH, '(//android.view.ViewGroup[@content-desc="Like button. Double tap and hold to react."])[1]').click()
-            time.sleep(3)
-            print(f"Liked : {self.link}")
-            super().__stop__()
-            return
+                self.driver.find_element(By.XPATH,
+                                         '(//android.view.ViewGroup[@content-desc="Like button. Double tap and hold to react."])[1]').click()
+                time.sleep(3)
+                print(f"Liked : {self.link}")
+                if random.random() < 0.05:
+                    LNT_COMMENT = ['còn ko ạ', 'gi']
+                    element = self.driver.find_element(By.XPATH, '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.EditText')
+                    element.click()
+                    element.send_keys(random.choice(COMMENTS))
+                    time.sleep(1)
+                    self.driver.find_element(By.XPATH,'//android.view.ViewGroup[@content-desc="Send"]').click()
 
 
-        except WebDriverException as e:
-            super().__stop__()
-            return
+
+            except WebDriverException as e:
+                super().__stop__()
+                return
+
+        super().__stop__()
+        return
